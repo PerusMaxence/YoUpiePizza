5cea7aaf2a63c19a6989440a5de2e4284c29e8f7
Perus Maxence <122904079+PerusMaxence@users.noreply.github.com>
2025-04-04 01:21:19 +0200

    Add files via upload

---
+++ v_mention_legal.php
@@ -0,0 +1,70 @@
+<?php
+include_once('view/head.php');
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Mentions Légales - Yo'Upie Pizza</title>
+</head>
+
+<body>
+
+    <header>
+        <?php include_once('view/header.php'); ?>
+    </header>
+    <main id="mention-main">
+        <section>
+            <h1><u>1. Informations légales</u></h1><br>
+            <h3>Le site Yo'Upie Pizza est édité par :</h3>
+            <p><strong>Nom de l'entreprise :</strong> Yo'Upie Pizza.</p>
+            <p><strong>Siège social :</strong> 23 Rue des Charrons - Montoison (26800).</p>
+            <p><strong>Responsable de la publication :</strong> M. Aurelien Etienne GUERIN.</p>
+            <p><strong>Numéro SIRET :</strong> 92868938900015.</p>
+            <p><strong>Numéro de TVA intracommunautaire :</strong> FR35928689389.</p>
+        </section><br>
+
+        <section>
+            <h1><u>2. Hébergeur :</u></h1><br>
+            <h3>Le site est hébergé par :</h3>
+            <p><strong>Nom de l'hébergeur :</strong> [Nom de l'hébergeur]</p>
+            <p><strong>Adresse de l'hébergeur :</strong> [Adresse complète de l'hébergeur]</p>
+            <p><strong>Contact de l'hébergeur :</strong> [Numéro de téléphone, email de l'hébergeur]</p>
+        </section><br>
+
+        <section>
+            <h1><u>3. Cookies et collecte de données personnelles :</u></h1><br>
+            <p>Ce site ne collecte aucune donnée personnelle sans votre consentement. Il n’utilise pas de cookies.</p>
+        </section><br>
+
+        <section>
+            <h1><u>4. Propriété intellectuelle :</u></h1><br>
+            <p>Tous les éléments du site (textes, images, logo, etc.) sont la propriété exclusive de Yo'Upie Pizza, sauf indication contraire. Toute reproduction ou représentation de ces éléments sans autorisation est interdite.</p>
+        </section><br>
+
+        <section>
+            <h1><u>5. Responsabilité :</u></h1><br>
+            <p>Yo'Upie Pizza ne saurait être tenue responsable des éventuels dommages directs ou indirects résultant de l'accès à ce site, de l'utilisation de ses services ou des erreurs qui pourraient y figurer.</p>
+        </section><br>
+
+        <section>
+            <h1><u>6. Modification des mentions légales :</u></h1><br>
+            <p>Les présentes mentions légales peuvent être modifiées à tout moment. Toute modification sera immédiatement applicable sur cette page.</p>
+        </section><br>
+
+        <section>
+            <h1><u>7.Contact :</u></h1><br>
+            <h3>Pour toute question relative à ces mentions légales ou à l'utilisation de ce site, vous pouvez nous contacter à :</h3>
+            <p><strong>Email :</strong> youpie.pizza.guerin@gmail.com.</p>
+            <p><strong>Téléphone :</strong> 04 75 82 97 30.</p>
+            <p><strong>Notre </strong> <a href="https://www.facebook.com/people/YoUpie-Pizza/61572196043078/">Facebook</a>.</p>
+        </section><br>
+    </main>
+
+    <footer>
+        <?php include_once('view/footer.php'); ?>
+    </footer>
+</body>
+
+</html>
